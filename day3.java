4112f5440046580671610b3d65c5d52a57d275b6
Lincoln Smith <MrSmartyMcFarty@gmail.com>
2021-12-02 18:50:10 -0600

    Setup for day 3 of Advent of Code 2021

---
+++ day3.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+
+public class day3 {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(new File("day3.txt"));
+    }
+}
